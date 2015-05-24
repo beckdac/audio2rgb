31e4d2d08af16fad87caa9f63233f5e384d6c711
Dave Beck <beck.dac@live.com>
2015-05-24 13:20:05 +0000

    fleshed out stub

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # system imports
+import os
 import argparse
 import tempfile
 
@@ -12,19 +13,21 @@ parser = argparse.ArgumentParser(description='audio2rgb CLI')
 parser.add_argument('input', help='filename of audio input')
 parser.add_argument("--verbose", help="enable verbose mode",
                             action="store_true")
+parser.add_argument("--debug", help="enable debug mode",
+                            action="store_true")
 
+# parser engage!
+args = parser.parse_args()
 
 # prepare environment
 
 # create temporary files
 tmpfp = tempfile.NamedTemporaryFile(delete=False)
-
-# parser engage!
-args = parser.parse_args()
-
+if args.debug:
+    print("tmp file: " + tmpfp.name)
 
 # handle any CLI argument preprocessing
-if (args.verbose):
+if args.verbose:
     print("verbose mode enabled")
 
 
@@ -32,3 +35,7 @@ if (args.verbose):
 
 
 # postprocessing
+# close and remove the tmp file
+tmpfp.close()
+if not args.debug:
+    os.unlink(tmpfp.name)
